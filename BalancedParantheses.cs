7ba72c2f330388fd1de1d5f2369a0f901eaff8e4
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-10 22:41:12 +0530

    [Tushar Vidwans] Added: Calculating Balanced of the Paranthesis on expressions

---
+++ BalancedParantheses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBalanceParanthesis
+{
+    /// <summary>
+    /// checks whether the expression is balanced parentheses or not. 
+    /// @param s is string input given by user 
+    /// @return boolean true if balanced else false.
+    /// </summary>
+    public class BalancedParentheses
+    {
+        
+        public bool balancedParentheses(String s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = s[i];
+                if (ch == '[' || ch == '(' || ch == '{')
+                {
+                    stack.Push(ch);
+                }
+                else if (ch == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+                else if (ch == ')')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+                else if (ch == '}')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '{')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleBalanceParanthesis
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the expression: ");
+            string exp = Console.ReadLine();
+            BalancedParentheses balancedParentheses = new BalancedParentheses();
+
+
+            if (balancedParentheses.balancedParentheses(exp))
+                Console.WriteLine("Balanced");
+            else
+                Console.WriteLine("not Balanced");
+        }
+    }
+}
